deb01cb50a196cc0a82c4fe1685f4af1e55cf58e
1257749706@qq.com <1257749706@qq.com>
2020-01-29 17:46:22 +0800

    Add:权限控制的前端显示控制

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -5,6 +5,12 @@
     $url=str_replace(strrchr($url, "?"),"",$url);
     $url = substr($url,0,strrpos($url,'.'));
 
+    //进行权限判断
+    $authUser = authUserManage();
+    $authProblem = authProblemManage();
+    $authContest = authContestManage();
+    $authNews = authNewsManage();
+
     $normal = false;        //常规栏
     $user_mg = false;       //用户管理
     $problem_mg = false;    //问题管理
@@ -27,7 +33,7 @@
     <div class="nano">
         <div class="nano-content">
             <ul>
-		<li><a href="/home" class=""><i class="ti-back-left"></i> 返回前台 </a></li>
+		        <li><a href="/home" class=""><i class="ti-back-left"></i> 返回前台 </a></li>
                 <li><a href="/admin" class=""><i class="ti-home"></i> 后台主页 </a></li>
                 <?php
                 if ($normal == true){?>
@@ -36,9 +42,13 @@
                 <li><a class="sidebar-sub-toggle"><i class="ti-layout-grid2"></i>常规设置  <span class="sidebar-collapse-icon ti-angle-down"></span></a>
                     <?php }?>
                     <ul>
-                        <li><a href="setindexmsg.php">首页公告</a></li>
                         <li><a href="newslist.php">公告列表</a></li>
-                        <li><a href="addnews.php">添加公告</a></li>
+                        <?php
+                            if ($authNews){?>
+                                <li><a href="setindexmsg.php">首页公告</a></li>
+                                <li><a href="addnews.php">添加公告</a></li>
+                         <?php   }
+                        ?>
                     </ul>
                 </li>
                 <?php
@@ -49,10 +59,13 @@
                         <?php }?>
                         <ul>
                             <li><a href="userlist.php">用户列表</a></li>
-                            <li><a href="importstudent.php">导入用户</a></li>
-                            <li><a href="privilegelist.php">权限列表</a></li>
-                            <li><a href="addprivilege.php">添加权限</a></li>
-
+                            <?php
+                                if ($authUser){?>
+                                    <li><a href="importstudent.php">导入用户</a></li>
+                                    <li><a href="privilegelist.php">权限列表</a></li>
+                                    <li><a href="addprivilege.php">添加权限</a></li>
+                             <?php   }
+                            ?>
                         </ul>
                     </li>
 
@@ -64,14 +77,14 @@
                     <?php }?>
                     <ul>
                         <li><a href="problemlist.php">问题列表</a></li>
-                        <li><a href="addproblem.php">添加问题</a></li>
-                        <li><a href="importproblem.php">批量导入问题</a></li>
+                        <?php
+                        if ($authProblem){?>
+                            <li><a href="addproblem.php">添加问题</a></li>
+                            <li><a href="importproblem.php">批量导入问题</a></li>
+                        <?php }?>
                         
                     </ul>
                 </li>
-
-                <!-- <li><a class="sidebar-sub-toggle"><i class="ti-search"></i>信息查询 </a>
-                 </li> -->
                 <?php
                     if ($conetst_mg == true){?>
                 <li class="active open"><a class="sidebar-sub-toggle"><i class="ti-cup"></i>竞赛&作业  <span class="sidebar-collapse-icon ti-angle-down"></span></a>
@@ -80,9 +93,13 @@
                     <?php }?>
                     <ul>
                         <li><a href="contestlist.php">竞赛&作业列表</a></li>
-                        <li><a href="addcontest.php">添加竞赛&作业</a></li>
-                        <li><a href="exportstucode.php">导出竞赛学生代码</a></li>
-<!--                        <li><a href="contestcreator.php">比赛队伍账号生成器</a></li>-->
+                        <?php
+                            if ($authContest){?>
+                                <li><a href="addcontest.php">添加竞赛&作业</a></li>
+                                <li><a href="exportstucode.php">导出竞赛学生代码</a></li>
+                                <!--                        <li><a href="contestcreator.php">比赛队伍账号生成器</a></li>-->
+                        <?php    }
+                        ?>
                     </ul>
                 </li>
 
